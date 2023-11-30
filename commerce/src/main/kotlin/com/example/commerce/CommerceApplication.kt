103d612761b6e6b00e56de97864e67290e287951
JBJ <maystar5987@gmail.com>
2023-11-30 11:30:27 +0900

    docker 설정 추가

--- commerce/src/main/kotlin/com/example/commerce/CommerceApplication.kt
+++ commerce/src/main/kotlin/com/example/commerce/CommerceApplication.kt
@@ -1,9 +1,12 @@
 package com.example.commerce
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -17,7 +20,10 @@ fun main(args: Array<String>) {
 	runApplication<CommerceApplication>(*args)
 }
 
+@Configuration
+class RedisConfig(private val redisTemplate:RedisTemplate<String, String>){
+	@PostConstruct
+	fun getConnection() {
 
-
-
-
+	}
+}
